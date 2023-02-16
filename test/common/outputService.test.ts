a858190e346e4c16bd7d946e3e6cc90136bae213
Juan Francisco Stenghele <jstenghele@salesforce.com>
2023-02-16 16:48:43 -0300

    test: updated and improved version of deploy output service

--- test/common/outputService.test.ts
+++ test/common/outputService.test.ts
@@ -8,7 +8,8 @@
 import { expect, test } from '@oclif/test';
 import * as sinon from 'sinon';
 import { Org } from '@salesforce/core';
-import { OutputService } from '../../src/common/outputService';
+import { DeployOutputService } from '../../src/common/outputService/deployOutputService';
+import { OutputServiceFactory } from '../../src/common/outputService/outputServiceFactory';
 import { AsyncOperationResult, ChangeBundleInstall, WorkItemPromote } from '../../src/common/types';
 import * as DeploySelector from '../../src/common/selectors/deployProgressSummarySelector';
 import * as EndpointSelector from '../../src/common/selectors/endpointSelector';
@@ -18,7 +19,7 @@ import * as ChangeBundleSelector from '../../src/common/selectors/changeBundleSe
 describe('outputService', () => {
   test.stdout().it('prints the async deploy execution correctly', (ctx) => {
     const mockId = 'ABC';
-    OutputService.printAsyncRun(mockId);
+    DeployOutputService.printAsyncRun(mockId);
     expect(ctx.stdout).to.contain(
       `Job ID: ${mockId}/n` +
         'Deploy has been queued./n' +
@@ -38,7 +39,7 @@ describe('outputService', () => {
       // eslint-disable-next-line camelcase
       sf_devops__Message__c: 'Message',
     };
-    OutputService.printAorStatus(aor);
+    DeployOutputService.printAorStatus(aor);
     expect(ctx.stdout).to.contain(status);
   });
 
@@ -54,7 +55,7 @@ describe('outputService', () => {
       // eslint-disable-next-line camelcase
       sf_devops__Message__c: message,
     };
-    OutputService.printDeploymentStatus(aor);
+    DeployOutputService.printDeploymentStatus(aor);
     expect(ctx.stdout).to.contain(message);
   });
 
@@ -70,7 +71,7 @@ describe('outputService', () => {
       // eslint-disable-next-line camelcase
       sf_devops__Message__c: message,
     };
-    OutputService.printDeploymentStatus(aor);
+    DeployOutputService.printDeploymentStatus(aor);
     expect(ctx.stdout).to.contain(message);
   });
 
@@ -85,7 +86,7 @@ describe('outputService', () => {
       // eslint-disable-next-line camelcase
       sf_devops__Message__c: 'Message',
     };
-    OutputService.printDeploymentStatus(aor);
+    DeployOutputService.printDeploymentStatus(aor);
     expect(ctx.stdout).to.contain('Deploy complete.');
   });
 
@@ -101,14 +102,29 @@ describe('outputService', () => {
       // eslint-disable-next-line camelcase
       sf_devops__Message__c: 'Message',
     };
-    OutputService.printDeploymentStatus(aor);
+    DeployOutputService.printDeploymentStatus(aor);
     expect(ctx.stdout).to.contain(`Deploy failed./n${errorDetails}`);
+  });
+
+  test.stdout().it('ignores an invalid aor status of a deployment when asked to print', (ctx) => {
+    const status = 'Invalid';
+    const aor: AsyncOperationResult = {
+      Id: 'Id',
+      // eslint-disable-next-line camelcase
+      sf_devops__Error_Details__c: undefined,
+      // eslint-disable-next-line camelcase
+      sf_devops__Status__c: status,
+      // eslint-disable-next-line camelcase
+      sf_devops__Message__c: '',
+    };
+    DeployOutputService.printDeploymentStatus(aor);
+    expect(ctx.stdout).to.be.empty;
   });
 
   describe('deployment summary', () => {
     let sandbox: sinon.SinonSandbox;
     const stubOrg = sinon.createStubInstance(Org);
-    const outputService: OutputService = new OutputService(stubOrg.getConnection());
+    const outputService: DeployOutputService = OutputServiceFactory.forDeployment(stubOrg.getConnection());
 
     beforeEach(() => {
       sandbox = sinon.createSandbox();
@@ -121,6 +137,22 @@ describe('outputService', () => {
     const workItem1Name = 'WI1';
     const workItem2Name = 'WI2';
     const stageName = 'UAT';
+    const branchName = 'testing';
+    const orgUrl = 'www.example.com';
+
+    test
+      .stdout()
+      .it('ignores a print deploy summary with a deployment that is not soup, versioned or ad hoc', async (ctx) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        sandbox.stub(DeploySelector, 'selectDeployAORSummaryData').resolves({
+          // eslint-disable-next-line camelcase
+          sf_devops__Work_Item_Promotes__r: null,
+          // eslint-disable-next-line camelcase
+          sf_devops__Change_Bundle_Installs__r: null,
+        });
+        await outputService.printProgressSummary('1234', branchName);
+        expect(ctx.stdout).to.be.empty;
+      });
 
     describe('ad hoc deployment', () => {
       function mockWorkItemPromote(name: string, stage: string, envNamedCredential: string): WorkItemPromote {
@@ -158,8 +190,7 @@ describe('outputService', () => {
       const mockWorkItemPromote1: WorkItemPromote = mockWorkItemPromote(workItem1Name, stageName, 'ABC');
       const mockWorkItemPromote2: WorkItemPromote = mockWorkItemPromote(workItem2Name, stageName, 'ABC');
 
-      test.stdout().it('prints the correct message for a ad hoc deployment', async (ctx) => {
-        const workItemsPromote: WorkItemPromote[] = [mockWorkItemPromote1, mockWorkItemPromote2];
+      async function adHocDeploy(workItemsPromote: WorkItemPromote[]): Promise<void> {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         sandbox.stub(DeploySelector, 'selectDeployAORSummaryData').resolves({
           // eslint-disable-next-line camelcase
@@ -172,11 +203,23 @@ describe('outputService', () => {
           // eslint-disable-next-line camelcase
           sf_devops__Change_Bundle_Installs__r: null,
         });
-        const orgUrl = 'www.example.com';
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         sandbox.stub(EndpointSelector, 'selectOrgUrl').resolves(orgUrl);
-        const branchName = 'testing';
         await outputService.printProgressSummary('1234', branchName);
+      }
+
+      test.stdout().it('prints the correct message for a single WI ad hoc deployment', async (ctx) => {
+        const workItemsPromote: WorkItemPromote[] = [mockWorkItemPromote1];
+        await adHocDeploy(workItemsPromote);
+        expect(ctx.stdout).to.contain(
+          `DevOps Center pipeline stage ${stageName} being updated with metadata associated with work item ` +
+            `${workItem1Name}. Deploying metadata from ${branchName} branch to target org ${orgUrl}.`
+        );
+      });
+
+      test.stdout().it('prints the correct message for a multiple WIs ad hoc deployment', async (ctx) => {
+        const workItemsPromote: WorkItemPromote[] = [mockWorkItemPromote1, mockWorkItemPromote2];
+        await adHocDeploy(workItemsPromote);
         expect(ctx.stdout).to.contain(
           `DevOps Center pipeline stage ${stageName} being updated with metadata associated with work items ` +
             `${workItem1Name}, ${workItem2Name}. Deploying metadata from ${branchName} branch to target org ${orgUrl}.`
@@ -238,7 +281,6 @@ describe('outputService', () => {
           },
         });
 
-        const orgUrl = 'www.example.com';
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         sandbox.stub(EndpointSelector, 'selectOrgUrl').resolves(orgUrl);
 
@@ -289,7 +331,6 @@ describe('outputService', () => {
           },
         ]);
 
-        const branchName = 'testing';
         await outputService.printProgressSummary('1234', branchName);
         expect(ctx.stdout).to.contain(
           `DevOps Center pipeline stage ${stageName} being updated with metadata associated with work items ` +
@@ -333,7 +374,6 @@ describe('outputService', () => {
           },
         });
 
-        const orgUrl = 'www.example.com';
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         sandbox.stub(EndpointSelector, 'selectOrgUrl').resolves(orgUrl);
 
@@ -372,7 +412,8 @@ describe('outputService', () => {
         });
 
         const workItems1 = [workItem1, workItem2];
-        const workItems2 = [workItem3, workItem4];
+        const workItems2 = [workItem3];
+        const workItems3 = [workItem4];
         sandbox.stub(ChangeBundleSelector, 'selectWorkItemsByChangeBundles').resolves([
           {
             Id: changeBundleInstall1Id,
@@ -392,13 +433,21 @@ describe('outputService', () => {
               records: workItems2,
             },
           },
+          {
+            Id: 'NotUsed',
+            // eslint-disable-next-line camelcase
+            sf_devops__Work_Items__r: {
+              done: true,
+              totalSize: workItems3.length,
+              records: workItems3,
+            },
+          },
         ]);
 
-        const branchName = 'testing';
         await outputService.printProgressSummary('1234', branchName);
         expect(ctx.stdout).to.contain(
           `DevOps Center pipeline stage ${stageName} being updated with metadata associated with work items ` +
-            `${workItem1Name}, ${workItem2Name} in bundle ${changeBundleInstall1VersionName}; work items ${workItem3Name}, ${workItem4Name} in bundle ${changeBundleInstall2VersionName}. ` +
+            `${workItem1Name}, ${workItem2Name} in bundle ${changeBundleInstall1VersionName}; work item ${workItem3Name} in bundle ${changeBundleInstall2VersionName}. ` +
             `Deploying metadata from ${branchName} branch to target org ${orgUrl}.`
         );
       });
